4cef8ce981c5b92428bbb7c463617d628c2fa2fe
Emmanuel Oluwatobi <56241351+Tee-py@users.noreply.github.com>
2024-07-28 12:24:50 +0100

    converted nav from a to Link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,12 @@
 import { Link } from "react-router-dom";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
-import { useState } from "react";
 import { useAccount } from "wagmi";
-// import { Link as ScrollLink } from "react-scroll";
 import { truncate } from "../utils/helper";
 
 const Navbar = () => {
   const { open } = useWeb3Modal();
-  const [activeLink, setActiveLink] = useState<string>("/");
   const { address } = useAccount();
 
-  // Handle click event
-  const handleLinkClick = (link: string) => {
-    setActiveLink(link);
-  };
   return (
     <div className="w-full z-10 fixed top-0 left-0 flex justify-center items-center px-7 bg-[#161616] border-b border-[#ffffff] h-[80px] ">
       <div className="max-w-[1200px] w-full flex justify-between items-center ">
@@ -23,28 +16,18 @@ const Navbar = () => {
           </Link>
           <div className="md:flex gap-2 hidden uppercase">
             {" "}
-            <a
-              onClick={() => handleLinkClick("/")}
-              href="/"
+            <Link
+              to="/"
               className="hover:text-[#00ECFF]"
             >
               Home
-            </a>{" "}
+            </Link>{" "}
             { address &&
-            <a
-              onClick={() => handleLinkClick("tokens")}
-              className={
-                activeLink === "tokens"
-                  ? "text-[#00ECFF] cursor-pointer "
-                  : "cursor-pointer"
-              }
-              href="/mytokens"
-              // to="tokens"
-              // smooth={true}
-              // duration={500}
+            <Link
+              to="/mytokens"
             >
               MyTokens
-            </a>}{" "}
+            </Link>}{" "}
           </div>
         </div>
         <div className="flex gap-3">
